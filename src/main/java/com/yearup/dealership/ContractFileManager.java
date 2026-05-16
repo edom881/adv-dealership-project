668858686998e814a58521f8e80162ffcfbbe89f
edomg <edomgezahegn54@gmail.com>
2026-05-16 17:31:18 -0500

    Save sales contracts to file

--- src/main/java/com/yearup/dealership/ContractFileManager.java
+++ src/main/java/com/yearup/dealership/ContractFileManager.java
@@ -11,6 +11,21 @@ public class ContractFileManager {
         try {
             FileWriter writer = new FileWriter(FILE_NAME, true);
 
+            if (contract instanceof SalesContract) {
+
+                SalesContract salesContract = (SalesContract) contract;
+
+                writer.write(
+                        "SALE|" +
+                                salesContract.getDate() + "|" +
+                                salesContract.getCustomerName() + "|" +
+                                salesContract.getCustomerEmail() + "|" +
+                                salesContract.getVehicleSold().toString() + "|" +
+                                salesContract.getTotalPrice() + "|" +
+                                salesContract.getMonthlyPayment() + "\n"
+                );
+            }
+
             writer.close();
 
         } catch (IOException e) {
