febba7902ae05993b2f4574326e95bba68aba607
edomg <edomgezahegn54@gmail.com>
2026-05-16 16:07:58 -0500

    Generate getters and setters

--- src/main/java/com/yearup/dealership/Contract.java
+++ src/main/java/com/yearup/dealership/Contract.java
@@ -5,6 +5,38 @@ public class Contract {
     protected String customerName;
     protected String customerEmail;
 
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public Vehicle getVehicleSold() {
+        return vehicleSold;
+    }
+
+    public void setVehicleSold(Vehicle vehicleSold) {
+        this.vehicleSold = vehicleSold;
+    }
+
     public Contract(String date, String customerName, String customerEmail, Vehicle vehicleSold) {
         this.date = date;
         this.customerName = customerName;
