b62314e5521dd6870d348e9a6c72f5165065c1be
edomg <edomgezahegn54@gmail.com>
2026-05-16 16:05:42 -0500

    Add constructor to Contract class

--- src/main/java/com/yearup/dealership/Contract.java
+++ src/main/java/com/yearup/dealership/Contract.java
@@ -4,6 +4,16 @@ public class Contract {
     protected String date;
     protected String customerName;
     protected String customerEmail;
+
+    public Contract(String date, String customerName, String customerEmail, Vehicle vehicleSold) {
+        this.date = date;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicleSold = vehicleSold;
+    }
+
     protected Vehicle vehicleSold;
 
-}
+
+    }
+
