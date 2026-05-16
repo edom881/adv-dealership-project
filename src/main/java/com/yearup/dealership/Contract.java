617a5ca0d5dda11a38e728832eada4de62b9bc88
edomg <edomgezahegn54@gmail.com>
2026-05-16 16:00:03 -0500

    Add fields to Contract class

--- src/main/java/com/yearup/dealership/Contract.java
+++ src/main/java/com/yearup/dealership/Contract.java
@@ -1,4 +1,9 @@
 package com.yearup.dealership;
 
 public class Contract {
+    protected String date;
+    protected String customerName;
+    protected String customerEmail;
+    protected Vehicle vehicleSold;
+
 }
