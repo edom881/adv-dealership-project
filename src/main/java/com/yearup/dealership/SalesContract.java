e84195f729111e8ccdad3be8ebbc322dc91bb878
edomg <edomgezahegn54@gmail.com>
2026-05-16 16:38:32 -0500

    complete Contract abstract class setup and Add SalesContract constructor and finance field

--- src/main/java/com/yearup/dealership/SalesContract.java
+++ src/main/java/com/yearup/dealership/SalesContract.java
@@ -1,4 +1,21 @@
 package com.yearup.dealership;
 
-public class SalesContract {
+public class SalesContract extends Contract {
+    private boolean finance;
+
+    public SalesContract(String date, String customerName,
+                         String customerEmail, Vehicle vehicleSold,
+                         boolean finance) {
+
+        super(date, customerName, customerEmail, vehicleSold);
+        this.finance = finance;
+    }
+
+    public boolean isFinance() {
+        return finance;
+    }
+
+    public void setFinance(boolean finance) {
+        this.finance = finance;
+    }
 }
