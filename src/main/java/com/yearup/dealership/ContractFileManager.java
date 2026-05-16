a78ddc4ace44e0626a2438457ba53988fe8ac8fc
edomg <edomgezahegn54@gmail.com>
2026-05-16 17:26:25 -0500

    Create saveContract method

--- src/main/java/com/yearup/dealership/ContractFileManager.java
+++ src/main/java/com/yearup/dealership/ContractFileManager.java
@@ -1,7 +1,22 @@
 package com.yearup.dealership;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class ContractFileManager {
 
     private static final String FILE_NAME = "contracts.csv";
+
+    public void saveContract(Contract contract) {
+
+        try {
+            FileWriter writer = new FileWriter(FILE_NAME, true);
+
+            writer.close();
+
+        } catch (IOException e) {
+            System.out.println("Error saving contract file.");
+        }
+    }
+
 
 }
